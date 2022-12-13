55214af4dd6be24972c5cb1cbf2fc8bb68855b62
Taha Siddiqui <tsiddiqui@touchbistro.com>
2022-12-13 00:40:35 -0600

    seeds

---
+++ src/databases/seeds/users.ts
@@ -0,0 +1,12 @@
+import { Knex } from 'knex';
+
+export async function seed(knex: Knex): Promise<void> {
+  // Deletes ALL existing entries
+
+  await knex('users').del();
+
+  // Inserts seed entries
+
+  await knex('users').insert([{ id: 1, email: 'abc@gmail.com', password: 'abc123' }]);
+}
+
