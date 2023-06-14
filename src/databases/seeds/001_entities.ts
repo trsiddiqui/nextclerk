e3293360b3836f2c5d4807da28350e47422a2ab0
majid <majid@nextclerk.com>
2023-06-14 10:42:23 -0400

    Add folder ID to entity seed

--- src/databases/seeds/001_entities.ts
+++ src/databases/seeds/001_entities.ts
@@ -11,6 +11,7 @@ export async function seed(knex: Knex): Promise<void> {
     {
       uuid: 'f590257b-a925-45d3-b980-26ff13faf64e',
       name: 'Entity 1',
+      folderId: '014JYSVASFZQYHW35AEJE3BWF4YHC5WRKM',
       isPrimary: true,
       createdBy: 'testUser',
       updatedBy: 'testUSer'
